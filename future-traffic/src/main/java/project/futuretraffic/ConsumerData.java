c3f54bf5317bbb5f05399e97e94429f504a54d39
Nuno Ferreira <nunof@ua.pt>
2020-05-21 23:49:50 +0100

    trying to fix this bs

--- future-traffic/src/main/java/project/futuretraffic/ConsumerData.java
+++ future-traffic/src/main/java/project/futuretraffic/ConsumerData.java
@@ -34,11 +34,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 
-
-
-@RestController // This means that this class is a Controller
-@RequestMapping(path="/api") // This means that the url for all API's in this controller start with /api
-public class MainController {
+@Service
+public class ConsumerData {
 
     private static final Logger log = LoggerFactory.getLogger(MainController.class);
 
@@ -53,12 +50,41 @@ public class MainController {
     @Autowired 
     private BusRepository busRepository;
 
-    // Get all buses
-    @CrossOrigin(origins = "http://192.168.160.103:6030/")
-    @GetMapping(path="/all")
-    public Iterable<Bus> getAllBuses(){
-        // This returns a JSON with all the buses        
-        return busRepository.findAll();
+    @PostConstruct
+    public void init(){
+        log.info("Initializing consumer controller");
+        con_properties.put("bootstrap.servers", "192.168.160.103:9092");
+        con_properties.put("key.deserializer", StringDeserializer.class.getName());
+        con_properties.put("value.deserializer" , StringDeserializer.class.getName());
+        con_properties.put("group.id" , "p22");
     }
+
+    @KafkaListener(topics="esp22", groupId="p22")
+    public void consume(String message){
+        String[] data = message.split(",");
+        int id = Integer.valueOf(data[0]);
+        String node_id = data[1];
+        int location_id = Integer.valueOf(data[2]);
+        float head = !data[3].equals("") ? Float.valueOf(data[3]) : 0;
+        float lon = Float.valueOf(data[4]);
+        float lat = Float.valueOf(data[5]);
+        int speed = !data[6].equals("") ? Integer.valueOf(data[6]) : 0;
+        String ts = data[7];
+        String write_time = data[8];
+
+        Bus b = new Bus(node_id, id, location_id, head, lon, lat, speed, ts, write_time);
+        log.info(b.toString());
+        busRepository.save(b);
+    }
+
+    // Overriding default boostrap server for the kafka listener callback stuff
+    @Bean
+    public ConsumerFactory<?, ?> kafkaConsumerFactory(KafkaProperties properties) {
+        Map<String, Object> props = properties.buildConsumerProperties();
+        props.put(org.apache.kafka.clients.CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG, "192.168.160.103:9092");
+        return new DefaultKafkaConsumerFactory<>(props);
+    }
+
+
 
 }
